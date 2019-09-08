c451e0e197bb3fabfa4b78b57d99e10c6fb69ef8
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-08 16:38:19 -0700

    rr

--- py-simple-recursion.py
+++ py-simple-recursion.py
@@ -3,11 +3,12 @@ def main():
     count_down(5)
 
 
+# recursive function that prints i until i = 0
 def count_down(i):
     print(i)
-    if i == 0:
+    if i == 0:      # base case
         return
-    else:
+    else:           # recursive case
         count_down(i - 1)
 
 
