e98a791d4bd7c295151af698ee055b208e376ac2
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-09 18:58:06 -0700

    rr

--- py-simple-recursion.py
+++ py-simple-recursion.py
@@ -1,5 +1,9 @@
-# recursive function that shows the base case
+# Sandro Aguilar
+# 09/2019
+# recursive functions that shows the base case
 # and recursive
+
+
 def main():
     print("Recursive subtraction:")
     count_down(5)
@@ -16,12 +20,15 @@ def count_down(i):
     else:           # recursive case
         count_down(i - 1)
 
-
+# a factorial is computed using recursion
 def factorial(x):
     if x == 1:
         return 1                        # base case
     else:
         return x * factorial(x - 1)     # recursive case
+
+
+
 
 
 # run program
